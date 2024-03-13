d69dad342a5e067380f577024d9e20889cc79acd
Aadhavan Arkhash Saravanakumar <arkhash.0309@gmail.com>
2024-03-13 18:48:45 +0530

    Updated commands as at 13th March.

---
+++ commands.sh
@@ -0,0 +1,26 @@
+pip install pandas
+pip install numpy
+pip install matplotlib
+pip install seaborn
+pip install scikit-learn
+pip install xgboost
+pip install keras
+pip install tensorflow
+pip install nltk
+pip install torch torchvision
+pip install spacy
+pip install pytorch-lightning
+pip install scikit-image
+pip install opencv-python
+pip install pillow
+pip install requests
+pip install beautifulsoup4
+pip install flask
+pip install django
+pip install fastapi
+pip install streamlit
+pip install jupyter
+pip install jupyterlab
+pip install boto3
+pip install google-cloud-storage
+pip install azure-storage-blob
